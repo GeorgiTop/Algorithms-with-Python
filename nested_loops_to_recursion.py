2b53fff72b3cceb0010a8df5abee612a5b4c1105
GeorgiTop <88086563+GeorgiTop@users.noreply.github.com>
2022-07-25 18:05:21 +0300

    Second task commit

---
+++ nested_loops_to_recursion.py
@@ -0,0 +1,14 @@
+from ast import For
+
+
+def nest_loop(idx, arr):
+    if idx == len(arr):
+        print(*arr)
+        return
+    for n in range(1, len(arr) + 1):
+        arr[idx] = n
+        nest_loop(idx+1, arr)    
+
+nest_size = int(input())
+nest = [None] * nest_size 
+nest_loop(0, nest)

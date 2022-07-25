b3887417182de7a2004b4a2c52e4aa2e3ecd84a6
GeorgiTop <88086563+GeorgiTop@users.noreply.github.com>
2022-07-25 20:53:48 +0300

    Task 3 (move_d/r) initial commit.

---
+++ move_down_or_right.py
@@ -0,0 +1,12 @@
+def get_paths(c, r, matrix):
+    count = 0
+    if c == len(matrix[0])-1 and r == len(matrix)-1:
+        return count
+    if matrix[c+1]
+    
+
+rows = int(input())
+cols = int(input())
+matrix = [[None] * cols for i in range(rows)]
+get_paths(0, 0, matrix)
+print(len(matrix))

d841b636736a1bfef7e4a4422186b51b293ac691
Georgi Toporchev <georgitoporchev@gmail.com>
2022-07-25 17:27:46 +0300

    Refactored module name

---
+++ reverse_array.py
@@ -0,0 +1,11 @@
+def reverse_array(idx, arr):
+    if idx == len(elements)//2:
+        return 
+    last_idx = len(elements) - 1 - idx
+    elements[idx], elements[last_idx] = elements[last_idx], elements[idx]
+    reverse_array(idx+1, arr)
+
+elements = input().split()
+
+reverse_array(0, elements)
+print(" ".join(elements))

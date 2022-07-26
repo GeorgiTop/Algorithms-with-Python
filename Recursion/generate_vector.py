5e3fa267659cad452ef650bf3eece2faf7f06102
GeorgiTop <88086563+GeorgiTop@users.noreply.github.com>
2022-07-26 15:35:36 +0300

    Generate_vector - Done

---
+++ Recursion/generate_vector.py
@@ -0,0 +1,12 @@
+def generate_vector(idx, arr):
+    if idx == len(arr):
+        print(*arr)
+        return
+    for n in range(0, 2):
+        vector[idx] = n
+        generate_vector(idx+1, arr)
+
+number = int(input())
+vector = [ None for _ in range(number)]
+
+generate_vector(0, vector)
